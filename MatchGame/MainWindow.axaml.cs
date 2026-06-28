812b735f725d5f6472cdbacce72ae86a99778d14
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2026-06-28 18:44:31 -0300

    feat: event PointerPressed

--- MatchGame/MainWindow.axaml.cs
+++ MatchGame/MainWindow.axaml.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
 
 namespace MatchGame;
 
 public partial class MainWindow : Window
 {
+    TextBlock lastTextBlock;
+    bool findingMatch = false;
+    
     public MainWindow()
     {
         InitializeComponent();
@@ -36,6 +41,29 @@ public partial class MainWindow : Window
             string nextEmoji = animalEmoji[index];
             textBlock.Text = nextEmoji;
             animalEmoji.RemoveAt(index);
+        }
+    }
+
+    private void TextBlock_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        TextBlock textBlock = sender as TextBlock;
+
+        if (!findingMatch)
+        {
+            textBlock.Foreground = Brushes.Crimson;
+            lastTextBlock = textBlock;
+            findingMatch = true;
+        }
+        else if (textBlock.Text == lastTextBlock.Text)
+        {
+            lastTextBlock.IsVisible = false;
+            textBlock.IsVisible = false;
+            findingMatch = false;
+        }
+        else
+        {
+            lastTextBlock.Foreground = textBlock.Foreground;
+            findingMatch = false;
         }
     }
 }
